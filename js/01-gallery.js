d543cfbd1c0c1d30469b5828bfc4640dd56b3193
gostraa <mcherkashina77@gmail.com>
2023-07-20 13:56:44 +0200

    start task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+    <a href="${original}" class="gallery__link">
+      <img
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        class="gallery__image"
+      />
+    </a>
+  </li>`;
+}).join('');
+
+galleryList.innerHTML = markup;
+
+//  делегування подій на img
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault(); 
+    if (event.target.tagName !== 'IMG') return;
+    const largeImageUrl = event.target.dataset.source;
+    const lightbox = basicLightbox.create(`<img src ="${largeImageUrl}">`);
+    
+    lightbox.show();
+    document.addEventListener("keydown", closeWithEsc)
+}
+    
+);
+
+function closeWithEsc(e) {
+    const modal = 
+    if(e.code === "Escape") {
+        modal.close();
+        document.removeEventListener("keydown", closeWithEsc)
+    }
+   
+}
